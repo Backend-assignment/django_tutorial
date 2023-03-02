d7f6b3ff9635d6082e7e80aa324d4a45e0f92e0f
ZARIFMAC <uzbzarif@gmail.com>
2023-03-02 09:48:37 +0500

    view info

--- api/views.py
+++ api/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse,HttpRequest,JsonResponse
 
 def index(request:HttpRequest):
-    data=request.GET
-    a = int(data.get('a',0))
-    b = int(data.get('b',0))
+    """
+    This is the index view get request
+    args:
+        request:HttpRequest
+    return:
+        HttpResponse
+    """
+    # get the data from the request
+    data=request.GET 
+    # get the value of a if not found return 0
+    a = int(data.get('a',0)) 
+    # get the value of b if not found return 0
+    b = int(data.get('b',0)) 
     return JsonResponse({'sum':a+b})
+
