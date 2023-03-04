e3a4683858c532dd65e0187a9ce97b12c1c580e2
ZARIFMAC <uzbzarif@gmail.com>
2023-03-04 14:32:45 +0500

    post

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse,HttpRequest,JsonResponse
+import json
 
 def index(request:HttpRequest):
     """
@@ -16,3 +17,25 @@ def index(request:HttpRequest):
     b = int(data.get('b',0)) 
     return JsonResponse({'sum':a+b})
 
+# Define the view
+
+def get_data(request:HttpRequest):
+    """
+    This is the get data view get request
+    args:
+        request:HttpRequest
+    return:
+        HttpResponse
+    """
+    # Check if the request is POST
+    if request.method == "POST":
+        data = request.body.decode()
+        print(json.loads(data))
+
+    # Check if the request is GET
+    if request.method == "GET":
+        print('GET')
+
+
+    return HttpResponse('Hello World')
+    
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 
 
 from django.urls import path
-from api.views import index
+from api.views import index, get_data
 
 
 urlpatterns = [
     path('', index),
+    path('get_data/', get_data),
 ]
