d9c9d5f109fd7f39f6e56e82ac768d906d012746
ZARIFMAC <uzbzarif@gmail.com>
2023-03-04 15:29:58 +0500

    add app to setting

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,11 @@
 
 from django.urls import path
 from api.views import index, get_data
+from django.contrib import admin
 
 
 urlpatterns = [
     path('', index),
+    path('admin/', admin.site.urls),
     path('get_data/', get_data),
 ]
